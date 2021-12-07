8fd287cf537eed1c434227d06eccdf906ba6e2ac
aleksandrgrek <goldoner@gmail.com>
2021-12-07 15:06:29 +0100

    auto update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import serial
 # print(ser.name)
 
 seri = serial.Serial()
-seri.baudrate = 3600
+seri.baudrate = 9600
 seri.port = '/dev/ttyUSB0'
 seri.parity = serial.PARITY_NONE
 seri.stopbits = 1
