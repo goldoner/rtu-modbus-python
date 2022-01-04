ab680896ca7b37b97323bde6e189d6949ac65134
aleksandrgrek <goldoner@gmail.com>
2022-01-04 23:05:58 +0300

    auto update

--- main.py
+++ main.py
@@ -1,58 +1,51 @@
-from time import sleep
-
 import easymodbus.modbusClient
 import serial
 
-# ser = serial.Serial('/dev/ttyUSB0')  # open serial port
-# print(ser.name)
-#
-# seri = serial.Serial()
-# seri.baudrate = 9600
-# seri.bytesize = 8
-# seri.parity = serial.PARITY_NONE
-# seri.stopbits = 1
-# seri.port = '/dev/ttyUSB0'
-#
-# if seri.isOpen():
-#     seri.close()
-# seri.open()
-# print(seri.is_open)
-# print(seri.is_open)
-# print(seri.is_open)
-#
-#
-#
-# print(seri.readlines())
-# sleep(2)
-# print(seri.readlines())
-# sleep(2)
-# print(seri.readlines())
-# sleep(2)
-# print(seri.readlines())
+import logging
+
+logging.basicConfig(filename="modbusPython.log",
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO)
+
 
 modbus_client = easymodbus.modbusClient.ModbusClient('/dev/ttyUSB0')
-
 
 modbus_client.baudrate = 9600
 modbus_client.parity = serial.PARITY_NONE
 modbus_client.stopbits = 8
 modbus_client.stopbits = 2
-#modbus_client = easymodbus.modbusClient.ModbusClient('/dev/ttyUSB0', seri)
 
 modbus_client.is_connected()
 
 modbus_client.connect()
-#
-# System.out.println("V1 L1: " + (float)
-# modbusClient.ReadHoldingRegisters(3530, 1)[0] / 10);
-#
-# The first argument is the starting registers, the second argument is the quantity.
 register_values = modbus_client.read_holdingregisters(4001, 1)
 
 print("Value of Register #1:" + str(register_values[0]))
 
-#
-# input_registers = modbus_client.read_inputregisters(0, 10)	#Read input registers 1 to 10 from server
-# holding_registers = modbus_client.read_holdingregisters(0, 5)	#Read holding registers 1 to 5 from server
+logging.info("Current REGISTER 4000 : " + modbus_client.read_holdingregisters(4001, 1)[0])
+logging.info("Voltage N-L REGISTER 4003 : " + modbus_client.read_holdingregisters(4004, 1)[0])
+logging.info("Voltage L-L REGISTER 4006 : " + modbus_client.read_holdingregisters(4007, 1)[0])
+logging.info("Real power REGISTER 4009 : " + modbus_client.read_holdingregisters(4010, 1)[0])
+
+logging.info("Apparent power REGISTER 4012 : " + modbus_client.read_holdingregisters(4013, 1)[0])
+logging.info("Reactive power REGISTER 4015 : " + modbus_client.read_holdingregisters(4016, 1)[0])
+logging.info("cos(phi) REGISTER 4018 : " + modbus_client.read_holdingregisters(4019, 1)[0])
+logging.info("Frequency REGISTER 4021 : " + modbus_client.read_holdingregisters(4022, 1)[0])
+
+logging.info("Real power, Sum REGISTER 4024 : " + modbus_client.read_holdingregisters(4025, 1)[0])
+logging.info("Real power EMAX REGISTER 4156 : " + modbus_client.read_holdingregisters(4157, 1)[0])
+logging.info("Apparent power, Sum REGISTER 4025 : " + modbus_client.read_holdingregisters(4026, 1)[0])
+logging.info("Reactive power, Sum REGISTER 4026 : " + modbus_client.read_holdingregisters(4027, 1)[0])
+logging.info("cos(phi), Sum REGISTER 4027 : " + modbus_client.read_holdingregisters(4028, 1)[0])
+logging.info("Current, Sum REGISTER 4028 : " + modbus_client.read_holdingregisters(4029, 1)[0])
+
+logging.info("Total harm dist factor _U REGISTER 4150 : " + modbus_client.read_holdingregisters(4151, 1)[0])
+logging.info("Total harm dist factor _I REGISTER 4153 : " + modbus_client.read_holdingregisters(4154, 1)[0])
+
+logging.info("Partial harmonic distortion _U REGISTER 4030 : " + modbus_client.read_holdingregisters(4031, 1)[0])
+logging.info("Partial harmonic distortion _I REGISTER 4090 : " + modbus_client.read_holdingregisters(4091, 1)[0])
+
 
 modbus_client.close()
---
+++ test.py
@@ -0,0 +1,18 @@
+import logging
+
+logging.basicConfig(filename="modbusPython.log",
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO)
+
+logging.info("Running Urban Planning")
+logging.info("Running Urban Planning")
+logging.info("Running Urban Planning")
+logging.info("Running Urban Planning")
+
+logging.error("AAAA")
+logging.error("AAAA")
+logging.error("AAAA")
+logging.error("AAAA")
+
