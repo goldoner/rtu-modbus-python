7eb0c4a7ed878b9129854d0a4930494127813706
aleksandrgrek <goldoner@gmail.com>
2022-01-06 19:46:24 +0300

    auto update

--- main.py
+++ main.py
@@ -57,30 +57,30 @@ modbus_client.connect()
 
 # logging.info(easymodbus.modbusClient.convert_registers_to_float(modbus_client.read_holdingregisters(3000, 1)[0]))
 
-# logging.info("REG  4000  : " + str(modbus_client.read_holdingregisters(4000, 1))[0])
-# logging.info("REG  4000  : " + str(modbus_client.read_discreteinputs(4000, 1))[0])
-# logging.info("REG  4000  : " + str(modbus_client.read_coils(4000, 1))[0])
+logging.info("REG  4000  : " + str(modbus_client.read_holdingregisters(4000, 1))[0])
+logging.info("REG  4000  : " + str(modbus_client.read_discreteinputs(4000, 1))[0])
+logging.info("REG  4000  : " + str(modbus_client.read_coils(4000, 1))[0])
+
+
+
+# logging.info("Voltage REGISTER 9100 SCALE : " + str(modbus_client.read_holdingregisters(9100, 1)))
+# logging.info("Voltage REGISTER 9101 SCALE : " + str(modbus_client.read_holdingregisters(9101, 1)))
 #
-
-
-logging.info("Voltage REGISTER 9100 SCALE : " + str(modbus_client.read_holdingregisters(9100, 1)))
-logging.info("Voltage REGISTER 9101 SCALE : " + str(modbus_client.read_holdingregisters(9101, 1)))
-
-logging.info("System Time REGISTER 3000 SCALE : " + str(modbus_client.read_holdingregisters(3001, 1)[0]))
-logging.info("AVG Time Current REGISTER 3001 SCALE : " + str(modbus_client.read_holdingregisters(3002, 6)[0]))
-
-logging.info("Current REGISTER 4000 : " + str(modbus_client.read_holdingregisters(4001, 3)[1]))
-logging.info("Current REGISTER 4000 : " + str(modbus_client.read_holdingregisters(4001, 3)[2]))
-
-logging.info("Voltage N-L REGISTER 4003 : " + str(modbus_client.read_holdingregisters(4004, 1)[0]))
-logging.info("Voltage L-L REGISTER 4006 : " + str(modbus_client.read_holdingregisters(4007, 1)[0]))
-logging.info("Real power REGISTER 4009 : " + str(modbus_client.read_holdingregisters(4010, 1)[0]))
-
-logging.info("Apparent power REGISTER 4012 : " + str(modbus_client.read_holdingregisters(4013, 1)[0]))
-logging.info("Reactive power REGISTER 4015 : " + str(modbus_client.read_holdingregisters(4016, 1)[0]))
-logging.info("cos(phi) REGISTER 4018 : " + str(modbus_client.read_holdingregisters(4019, 1)[0]))
-logging.info("Frequency REGISTER 4021 : " + str(modbus_client.read_holdingregisters(4022, 1)[0]))
+# logging.info("System Time REGISTER 3000 SCALE : " + str(modbus_client.read_holdingregisters(3001, 1)[0]))
+# logging.info("AVG Time Current REGISTER 3001 SCALE : " + str(modbus_client.read_holdingregisters(3002, 6)[0]))
 #
+# logging.info("Current REGISTER 4000 : " + str(modbus_client.read_holdingregisters(4001, 3)[1]))
+# logging.info("Current REGISTER 4000 : " + str(modbus_client.read_holdingregisters(4001, 3)[2]))
+#
+# logging.info("Voltage N-L REGISTER 4003 : " + str(modbus_client.read_holdingregisters(4004, 1)[0]))
+# logging.info("Voltage L-L REGISTER 4006 : " + str(modbus_client.read_holdingregisters(4007, 1)[0]))
+# logging.info("Real power REGISTER 4009 : " + str(modbus_client.read_holdingregisters(4010, 1)[0]))
+#
+# logging.info("Apparent power REGISTER 4012 : " + str(modbus_client.read_holdingregisters(4013, 1)[0]))
+# logging.info("Reactive power REGISTER 4015 : " + str(modbus_client.read_holdingregisters(4016, 1)[0]))
+# logging.info("cos(phi) REGISTER 4018 : " + str(modbus_client.read_holdingregisters(4019, 1)[0]))
+# logging.info("Frequency REGISTER 4021 : " + str(modbus_client.read_holdingregisters(4022, 1)[0]))
+# #
 # logging.info("Real power, Sum REGISTER 4024 : " + str(modbus_client.read_holdingregisters(4025, 1)[0]))
 # logging.info("Real power EMAX REGISTER 4156 : " + str(modbus_client.read_holdingregisters(4157, 1)[0]))
 # logging.info("Apparent power, Sum REGISTER 4025 : " + str(modbus_client.read_holdingregisters(4026, 1)[0]))
